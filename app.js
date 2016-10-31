db359e32d789f0a1569c784ac743bd866fe0570b
Satya & Ian <satyakra@buffalo.edu>
2016-10-31 01:08:16 -0400

    Final push for sprint.

--- app.js
+++ app.js
@@ -108,12 +108,28 @@
 
     var userID = auth.currentUser.uid();
     var list = [];
+    var slist = [];
+    var ilist = [];
     var userRole;
     var sselect = document.getElementById("scourses");
     var iselect = document.getElementById("icourses");
-    const dbObject = firebase.database().ref().child('users/' + userid);
+    const dbObject = firebase.database().ref().child('users/' + userID);
     const dbCurrentUserCourses = dbObject.child('courses');
+    const dbStuCourses = dbCurrentUserCourses.child('StudentCourses');
+    const dbInstCourses =  dbCurrentUserCourses.child('InstCourses');
     const dbCurrentUserRole = dbObject.child('Role');
+
+    dbStuCourses.on('child_added', snapshot => {
+
+        slist = snapshot.val();
+
+    });
+
+    dbInstCourses.on('child_added', snapshot => {
+
+        ilist = snapshot.val();
+
+    });
 
     dbCurrentUserCourses.on('child_added', snapshot => {
 
@@ -136,13 +152,31 @@
         }
     }
 
-    if(userRole == 1){
+    if(userRole == 2){
         for(var i = 0; i < list.length; i++) {
             var opt = document.createElement('option');
             opt.innerHTML = list[i];
             opt.value = list[i];
             iselect.appendChild(opt);
         }
+    }
+
+    if(userRole == 1){
+
+        for(var i = 0; i < slist.length; i++) {
+            var opt = document.createElement('option');
+            opt.innerHTML = slist[i];
+            opt.value = slist[i];
+            sselect.appendChild(opt);
+        }
+
+        for(var i = 0; i < ilist.length; i++) {
+            var opt = document.createElement('option');
+            opt.innerHTML = ilist[i];
+            opt.value = ilist[i];
+            iselect.appendChild(opt);
+        }
+
     }
 
     /*
